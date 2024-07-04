ab0f190f39c4ad9c75d931fac45ad3d32cefb1c1
abdulmoizbhatti <abdulmoiz.bhatti@ucalgary.ca>
2024-07-04 04:29:50 -0600

    Changed World to Moiz

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+/*
+ * File Name: Application.java
+ * Assignment: Lab 1 Exercise B
+ * Completed by: Moiz Bhatti
+ * Submission Date: July 4th, 2024
+ */
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello, World!");
+	}
+
+}
